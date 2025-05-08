017b0d77e105782f538a7accc6cdcc8490d9a6db
Ava Abadian <avaabadian@Mac.lan>
2025-05-08 04:07:01 -0700

    Fix eslint config for Vercel build

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,12 @@
+import eslintPluginReact from 'eslint-plugin-react';
+
 export default [
   {
+    plugins: {
+      react: eslintPluginReact,
+    },
     rules: {
-      "react/no-unescaped-entities": "off",
+      'react/no-unescaped-entities': 'off',
     },
   },
 ];
