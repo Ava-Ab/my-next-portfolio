ecd25c45f4ce2b0917a7e00573b129c0852664a4
Ava Abadian <avaabadian@Mac.lan>
2025-05-08 02:31:34 -0700

    Fix quotes for Vercel deployment

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <h1>Hi, I'm Ava 👋</h1>
-      <p>I’m a Cognitive Science major at UCSD with specialization in design and interaction.</p>
+      <p>I'm a Cognitive Science major at UCSD with specialization in design and interaction.</p>
     </main>
   );
 }
